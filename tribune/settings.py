99cad46889a8aa237d112e85e2d3fe02f221a3c3
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-04 21:46:10 +0300

    updated the settings file to add static files

--- tribune/settings.py
+++ tribune/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news',
+    'news.apps.NewsConfig',
+    # 'news',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -126,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
