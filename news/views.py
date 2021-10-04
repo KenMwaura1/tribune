c12328cbe818e47916916122bb1ecf7cb9a22fe4
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-04 21:47:12 +0300

    removed hardcoded html replaced with templates

--- news/views.py
+++ news/views.py
@@ -1,20 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 import datetime as dt
 
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to Zoo tribune")
+    return render(request, 'welcome.html')
 
 
 def news_of_day(request):
     date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/today-news.html', {'date': date})
+
+
+def convert_dates(dates):
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', "Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+
+# View Function to present news from past days
+def past_days_news(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
