889d9eaf946e7997a69b875e5d9ad5e62401fb44
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-04 15:07:45 +0300

    updated the urls the news urls and views

--- news/views.py
+++ news/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime as dt
+
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse("Welcome to Zoo tribune")
+
+
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
