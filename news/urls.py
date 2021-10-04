c12ff16bfc4dae75a968d4feb6a18661b594821d
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-04 22:18:11 +0300

    Updated the urls for previous dates, link for today news template and past news template.

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.welcome, name='home'),
-    url('^today/$',views.news_of_day,name='newsToday')
+    url('^today/$', views.news_of_day, name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews')
 ]
