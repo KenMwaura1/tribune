257edf1e3a7c13bf3e26ab4a126a463e6dffe4eb
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-04 15:06:57 +0300

    updated the settings to use PostgreSQL

--- tribune/settings.py
+++ tribune/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -76,11 +80,14 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': f'{os.environ.get("POSTGRES_DB_NAME")}',
+        'USER': f'{os.environ.get("POSTGRES_USER")}',
+        'PASSWORD': f'{os.environ.get("POSTGRES_PASSWORD")}',
+        'HOST': f'{os.environ.get("POSTGRES_DB_HOST")}',
+        'PORT': f'{os.environ.get("POSTGRES_DB_PORT")}',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
