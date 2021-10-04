435ec19db7ee30c259f3cf44ed2414cd2103f17c
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-05 00:42:03 +0300

    fixed typos

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kf_ial&*3ng4jp9@*+)mkrc6dod$o$myhmu_jq67*84&-x%smv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
