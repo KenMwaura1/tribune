f9858721c1449a8f3513817cb59a7f9281eaed35
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-05 20:10:47 +0300

    Updated the urls to add a root url and migrations

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.welcome, name='home'),
+    # url(r'^$', views.welcome, name='home'),
     url('^today/$', views.news_of_day, name='newsToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews')
 ]
