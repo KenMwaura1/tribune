02dc43b082fa2ef0e20efcb2128a9cd63f7c3395
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-05 20:11:26 +0300

    Updated the admin file to register models

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article, Editor, Tags
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Editor)
+admin.site.register(Tags)
+
