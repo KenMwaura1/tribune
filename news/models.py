5e480c6a12f1a09050d5161275a9c09e98049879
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-05 14:37:43 +0300

    Updated the Editor model, time settings and made migrations

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.template.defaultfilters import first
+
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name']
