c901c07d525686b2e050cd7b64c12bf6635d7e72
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-05 21:13:40 +0300

    Updated the tests to query news today and specified dates

--- news/tests.py
+++ news/tests.py
@@ -1,12 +1,26 @@
 from django.test import TestCase
 from .models import Article, Editor, Tags
-
+import datetime as dt
 
 # Create your tests here.
 class EditorTestClass(TestCase):
     # Setup method
     def setUp(self):
         self.zoo = Editor(first_name="Zoo", last_name="vier", email="zoo@test.com")
+        self.zoo.save_editor()
+
+        # Creating and saving a new tag
+        self.new_tag = Tags(name="testing")
+        self.new_tag.save()
+
+        self.new_article = Article(title="Test Article", post=" This is a random test post", editor=self.zoo)
+        self.new_article.save()
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        Tags.objects.all().delete()
+        Article.objects.all().delete()
 
     def test_instance(self):
         self.assertTrue(isinstance(self.zoo, Editor))
@@ -36,3 +50,13 @@ class EditorTestClass(TestCase):
         self.zoo.save_editor()
         self.zoo.update_editor_email("kemwaura@gmail.com")
         self.assertTrue(self.zoo.email == "kemwaura@gmail.com")
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
