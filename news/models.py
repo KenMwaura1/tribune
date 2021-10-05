990da20696b52f02cebc43bcb0dea2d51ea5c1fc
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-05 15:02:18 +0300

    Updated the Article and Tags Models

--- news/models.py
+++ news/models.py
@@ -14,3 +14,30 @@ class Editor(models.Model):
 
     class Meta:
         ordering = ['first_name']
+
+    def save_editor(self):
+        self.save()
+
+    def delete_editor(self):
+        self.delete()
+
+    @staticmethod
+    def show_all_editors():
+        return Editor.objects.all()
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags)
+    pub_date = models.DateField(auto_now_add=True)
+
+
