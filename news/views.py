0ebded890c9a0fa41a6d31021579b4b887d184a1
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-06 21:35:07 +0300

    Fixed typos

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404
 import datetime as dt
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from django.shortcuts import render, redirect
+
 from .models import Article
 
 
@@ -12,7 +14,7 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {'date': date, "news":news})
+    return render(request, 'all-news/today-news.html', {'date': date, "news": news})
 
 
 def convert_dates(dates):
@@ -28,7 +30,6 @@ def convert_dates(dates):
 
 # View Function to present news from past days
 def past_days_news(request, past_date):
-
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
@@ -36,7 +37,7 @@ def past_days_news(request, past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-        assert False
+        # assert False
 
     if date == dt.date.today():
         return redirect(news_of_day)
@@ -50,17 +51,16 @@ def search_results(request):
         searched_articles = Article.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'all-news/search.html', {"message": message, "articles": searched_articles})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message})
+        return render(request, 'all-news/search.html', {"message": message})
 
 
 def article(request, article_id):
     try:
         article = Article.objects.get(id=article_id)
-    except DoesNotExist:
+    except ObjectDoesNotExist:
         raise Http404()
     return render(request, "all-news/article.html", {'article': article})
-
