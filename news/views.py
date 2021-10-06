3648e6bcee6e67f98b0144122a97d616ec4e01db
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-06 18:28:29 +0300

    Created the functions to search and return single articles

--- news/views.py
+++ news/views.py
@@ -43,3 +43,24 @@ def past_days_news(request, past_date):
     news = Article.days_news(date)
     return render(request, 'all-news/past-news.html', {"date": date, "news": news})
 
+
+def search_results(request):
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
+
+def article(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, "all-news/article.html", {'article': article})
+
