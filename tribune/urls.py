ff49e28fdc22ae55f3aa0f5153509ccd22b1c806
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-07 12:51:58 +0300

    heroku deployment

--- tribune/urls.py
+++ tribune/urls.py
@@ -1,4 +1,4 @@
-"""tribune URL Configuration
+"""zoo-tribune URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
