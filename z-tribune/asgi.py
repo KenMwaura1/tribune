5da04f167c32f34eef6aca42a6d3efdd2623fea0
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-07 13:57:12 +0300

    heroku deployment

--- z-tribune/asgi.py
+++ z-tribune/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for z-tribune project.
+ASGI config for tribune project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'z-tribune.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tribune.settings')
 
 application = get_asgi_application()
--- z-tribune/wsgi.py
+++ z-tribune/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for z-tribune project.
+WSGI config for tribune project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'z-tribune.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tribune.settings')
 
 application = get_wsgi_application()
