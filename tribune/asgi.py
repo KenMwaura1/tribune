f6319c31d4e9662b45d66667ef84c632ce7100d7
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-07 13:01:43 +0300

    project-name updates

--- tribune/asgi.py
+++ tribune/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for zoo-tribune project.
+ASGI config for tribune project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zoo-tribune.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tribune.settings')
 
 application = get_asgi_application()
