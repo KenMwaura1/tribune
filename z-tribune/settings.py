278806ac6f4f470657938fbb8c09594ce06f946f
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 23:56:16 +0300

    Updated the base template

--- z-tribune/settings.py
+++ z-tribune/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news.apps.NewsConfig',
-    # 'news',
+    # 'news.apps.NewsConfig',
+    'news',
     'bootstrap5'
 ]
 
