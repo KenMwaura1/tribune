4b11339c0b4d74b45ebe1848f47b1cbbe4e05de8
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-11 23:33:53 +0300

    Added routes for django registration.

--- z-tribune/urls.py
+++ z-tribune/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('news.urls'))
+    path('', include('news.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
