b252b7728cbaaf1358593a9d4fb42827e5abac1e
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-12 14:18:23 +0300

    added tiny mce editor

--- z-tribune/urls.py
+++ z-tribune/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', LogoutView, {"next_page": '/'}),
     # path('accounts/login', )
+    path('tinymce/', include('tinymce.urls')),
 ]
+
