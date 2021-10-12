af02f55a10e9e2d4edc535b72776ec0318e68012
Kennedy-Mwaura <kemwaura@gmail.com>
2021-10-12 14:17:49 +0300

    Created the route for new article and the template

--- news/views.py
+++ news/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .email import send_welcome_email
-from .forms import NewsLetterForm
+from .forms import NewsLetterForm, NewArticleForm
 from .models import Article, NewsLetterRecipients
 
 
@@ -80,3 +80,19 @@ def article(request, article_id):
     except ObjectDoesNotExist:
         raise Http404()
     return render(request, "all-news/article.html", {'article': article})
+
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
